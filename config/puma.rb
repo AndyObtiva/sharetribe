87632e8921c9170fc2805470a1f38ff4de87f042
AndyObtiva <andy.am@gmail.com>
2017-02-18 03:23:41 +0400

    Downnumbered puma threads to 1 locally

---
+++ config/puma.rb
@@ -0,0 +1,17 @@
+if ENV['RAILS_ENV'] == 'production'
+  plugin "heroku"
+  on_worker_boot do
+   ActiveSupport.on_load(:active_record) do
+     ActiveRecord::Base.establish_connection
+   end
+  end
+  before_fork do
+   ActiveRecord::Base.connection_pool.disconnect!
+  end
+else
+  threads 1,1
+  workers 4
+end
+if ENV['PUMA_PRELOAD_APP'].to_s.downcase == 'true'
+  preload_app!
+end
