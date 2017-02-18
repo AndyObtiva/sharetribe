6ad749f8442991797df67a9d0c047f500e1e73ba
AndyObtiva <andy.am@gmail.com>
2017-02-19 02:55:01 +0400

    Added ability to clear sample seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
+# Always clear sample seeds
+Category.joins(:translations).where("category_translations.name like '% Category'").destroy_all
+CategoryTranslation.where("name like '% Category'").destroy_all
+Listing.where("title like '%I can ship%'").destroy_all
+Person.where("phone_number like '%555'").destroy_all
+
 if ENV['SAMPLE_SEEDS'].to_s.downcase == 'true'
   require Rails.root.join('lib', 'factories.rb').to_s
-
-  Category.joins(:translations).where("category_translations.name like '% Category'").destroy_all
-  CategoryTranslation.where("name like '% Category'").destroy_all
-  Listing.where("title like '%I can ship%'").destroy_all
-  Person.where("phone_number like '%555'").destroy_all
 
   10.times { FactoryGirl.create(:seed_category) }
   100.times { FactoryGirl.create(:seed_listing) }
