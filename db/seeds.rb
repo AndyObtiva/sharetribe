bc33f7e9315d79dbd81bdbb3722cbbd67235e53f
AndyObtiva <andy.am@gmail.com>
2017-02-18 23:44:35 +0400

    Required factories in seeds for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 if ENV['SAMPLE_SEEDS'].to_s.downcase == 'true'
+  require Rails.root.join('lib', 'factories.rb').to_s
+
   Category.joins(:translations).where("category_translations.name like '% Category'").destroy_all
   CategoryTranslation.where("name like '% Category'").destroy_all
   Listing.where("title like '%I can ship%'").destroy_all
