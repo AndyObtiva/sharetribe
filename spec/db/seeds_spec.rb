197becdfa3746a22b80ca53064e376a4b278bbab
AndyObtiva <andy.am@gmail.com>
2017-02-20 22:47:29 +0400

    SEED_COUNT env var usage

--- spec/db/seeds_spec.rb
+++ spec/db/seeds_spec.rb
@@ -28,21 +28,22 @@ describe 'Seeds' do
     # Run twice to ensure it refreshes data and does not destroy data available
     # before seeding
     ENV['SAMPLE_SEEDS'] = 'true'
+    ENV['SEED_COUNT'] = '10' #small to speed up tests
     load Rails.root.join('db', 'seeds.rb').to_s
     load Rails.root.join('db', 'seeds.rb').to_s
   end
-  it 'adds 10 categories and SEED_COUNT listings with their associations' do
+  it "adds 10 categories and #{ENV['SEED_COUNT']} listings with their associations" do
     # expect(ListingShape::HABTM_Categories.count).to eq(initial_category_listing_shape_count + 10)
     # expect(Category::HABTM_ListingShapes.count).to eq(initial_category_listing_shape_count + 10)
     expect(ListingShape.count).to eq(initial_listing_shape_count + 1)
     expect(Category.count).to eq(initial_category_count + 10)
     expect(CategoryTranslation.count).to eq(initial_category_translation_count + 10)
-    expect(Listing.count).to eq(initial_listing_count + SEED_COUNT)
-    expect(ListingImage.count).to eq(initial_listing_image_count + SEED_COUNT)
-    expect(Location.count).to eq(initial_listing_count + 200) #origins and destinations
-    expect(Person.count).to eq(initial_person_count + SEED_COUNT)
-    expect(CommunityMembership.count).to eq(initial_person_count + SEED_COUNT)
-    expect(Email.count).to eq(initial_email_count + SEED_COUNT)
+    expect(Listing.count).to eq(initial_listing_count + ENV['SEED_COUNT'].to_i)
+    expect(ListingImage.count).to eq(initial_listing_image_count + ENV['SEED_COUNT'].to_i)
+    expect(Location.count).to eq(initial_listing_count + 2*ENV['SEED_COUNT'].to_i) #origins and destinations
+    expect(Person.count).to eq(initial_person_count + ENV['SEED_COUNT'].to_i)
+    expect(CommunityMembership.count).to eq(initial_person_count + ENV['SEED_COUNT'].to_i)
+    expect(Email.count).to eq(initial_email_count + ENV['SEED_COUNT'].to_i)
 
     initial_excluded_model_counts.each do |excluded_model, initial_excluded_model_count|
       expect("#{excluded_model} count: #{excluded_model.count}").to eq("#{excluded_model} count: #{initial_excluded_model_count}")
