a217836358963bd3661baed0641391fcbb288cb2
AndyObtiva <andy.am@gmail.com>
2017-02-25 02:46:10 +0400

    Improved migration

--- db/migrate/20141222130455_remove_failed_invitation_created_jobs.rb
+++ db/migrate/20141222130455_remove_failed_invitation_created_jobs.rb
@@ -1,6 +1,6 @@
 class RemoveFailedInvitationCreatedJobs < ActiveRecord::Migration
   def up
-    execute("DELETE FROM `delayed_jobs` WHERE attempts = 3 AND failed_at IS NOT NULL AND `handler` LIKE '%ruby/struct:InvitationCreatedJob%'")
+    execute("DELETE FROM `delayed_jobs` WHERE (attempts = 3) AND (failed_at IS NOT NULL) AND (`handler` LIKE '%ruby/struct:InvitationCreatedJob%')")
   end
 
   def down
