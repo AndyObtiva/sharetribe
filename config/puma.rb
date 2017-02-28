cb77f368530982c6b2b8677126b0074fcb8c9647
AndyObtiva <andy.am@gmail.com>
2017-02-28 10:30:36 +0400

    Puma staging and production configuration of threads/workers via ENV

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
-if ENV['RAILS_ENV'] == 'production'
+if ['production', 'staging'].include?(ENV['RAILS_ENV'].to_s.downcase)
   plugin "heroku"
   on_worker_boot do
    ActiveSupport.on_load(:active_record) do
@@ -8,8 +8,8 @@ if ENV['RAILS_ENV'] == 'production'
   before_fork do
    ActiveRecord::Base.connection_pool.disconnect!
   end
-  threads 1,1
-  workers 1
+  threads (ENV['PUMA_THREADS_MIN'] || 1).to_i,(ENV['PUMA_THREADS_MAX'] || 1).to_i
+  workers (ENV['PUMA_WORKERS'] || 1).to_i
 else
   threads 1,1
   workers 4
